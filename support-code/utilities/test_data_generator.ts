5cea234d5db099a3a505651391f4890c31e01159
Hein-Carl Frijlink <hc.frijlink@freedom.nl>
2023-12-06 10:09:23 +0100

    update packages, update deprecated faker methods

--- support-code/utilities/test_data_generator.ts
+++ support-code/utilities/test_data_generator.ts
@@ -7,18 +7,24 @@ export default class TestDataGenerator {
     }
 
     public static generatePerson() {
-       return {
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName(),
-        email: faker.internet.email(),
-        telephone: faker.phone.number('06#########'),
-        password: "test",
-        address: faker.address.streetAddress(),
-        postalCode: faker.address.zipCode(),
-        city: faker.address.cityName(),
-        country: "Netherlands",
-        province: "Utrecht"
-       }
+        return {
+            firstName: faker.person.firstName(),
+            lastName: faker.person.lastName(),
+            email: faker.internet.email(),
+            telephone: `06${this.getRandomInteger(10000000, 99999999)}`,
+            password: "test",
+            address: faker.location.streetAddress(),
+            postalCode: faker.location.zipCode(),
+            city: faker.location.city(),
+            country: "Netherlands",
+            province: "Utrecht"
+        }
+    }
+
+    public static getRandomInteger(min: number, max: number) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
     }
 
 }
